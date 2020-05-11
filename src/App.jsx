86608e4d873ccbb7101006fdaa8ae32978047205
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-05-12 00:48:10 +0900

    material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,16 @@ export default class App extends React.Component {
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch(true) {
       case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500)
         break;
+          
+        case (/^https:*/.test(nextQuestionId)):
+          const a = document.createElement('a');
+          a.href = nextQuestionId;
+          a.target = '_blank';
+          a.click();
+        break;
+
       default:
         const chats = this.state.chats;
         chats.push({
@@ -45,7 +53,7 @@ export default class App extends React.Component {
          chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 1000);
         break;
     }
   }
@@ -55,6 +63,13 @@ export default class App extends React.Component {
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const scrollArea = document.getElementById('scroll-area');
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight
+    }
   }
 
   render() {
